e4de5f53947fd1c5c80f099055b80309713240d4
Radiosquirrel <radio_squirrel@mail.ru>
2018-10-15 16:20:14 +0300

    Урок 6. Продвинутое ООП

---
+++ Lesson_6/Homework_6.java
@@ -0,0 +1,64 @@
+/**
+ * Java. Level 1. Lesson 6. Homework 6
+ * @author Maya Plieva
+ * @version Sep 30, 2018
+ */
+
+class Homework_6 {
+
+    public static void main(String[] args) {
+        Animal[] animals = {new Cat(200, 2), new Dog(500, 0.5f, 10)};
+        for (Animal animal : animals)
+            System.out.println(
+                animal.getClass().getName() +
+                "\nrun: " + animal.run(200) +
+                "\njump: " + animal.jump(1.5f) +
+                "\nswim: " + animal.swim(5));
+    }
+}
+
+class Cat extends AnimalImpl {
+    Cat(int run_limit, float jump_limit, int swim_limit) {
+        super(run_limit, jump_limit, swim_limit);
+    }
+    Cat(int run_limit, float jump_limit) {
+        super(run_limit, jump_limit, -1);
+    }
+    @Override
+    public boolean swim(int distance) {
+        return false; // cats cannot swim (by the condition of task)
+    }
+}
+
+class Dog extends AnimalImpl {
+    Dog(int run_limit, float jump_limit, int swim_limit) {
+        super(run_limit, jump_limit, swim_limit);
+    }
+}
+
+interface Animal {
+    boolean run(int distance);
+    boolean jump(float height);
+    boolean swim(int distance);
+}
+
+abstract class AnimalImpl implements Animal {
+    protected int run_limit;
+    protected float jump_limit;
+    protected int swim_limit;
+
+    AnimalImpl(int run_limit, float jump_limit, int swim_limit) {
+        this.run_limit = run_limit;
+        this.jump_limit = jump_limit;
+        this.swim_limit = swim_limit;
+    }
+    public boolean run(int distance) {
+        return distance <= run_limit;
+    }
+    public boolean jump(float height)  {
+        return height <= jump_limit;
+    }
+    public boolean swim(int distance) {
+        return distance <= swim_limit;
+    }
+}
---
+++ Lesson_6/Lesson_6.java
@@ -0,0 +1,48 @@
+class Lesson_6 {
+
+    public static void main(String[] args) {
+        IAnimal[] animals = {new Cat("Murzik", "white", 3),
+            new Dog("Barbos", "black", 11)};
+        for (IAnimal animal : animals)
+            System.out.println(animal + " " + animal.voice());
+    }
+}
+
+class Cat extends Animal {
+    Cat(String name, String color, int age) {
+        super(name, color, age);
+    }
+    public String voice() {
+        return "meow";
+    }
+}
+
+class Dog extends Animal {
+    Dog(String name, String color, int age) {
+        super(name, color, age);
+    }
+    public String voice() {
+        return "wuf-wuf";
+    }
+}
+
+interface IAnimal {
+    String voice();
+}
+
+abstract class Animal implements IAnimal {
+    protected String name;
+    protected String color;
+    protected int age;
+
+    Animal(String name, String color, int age) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + name + ", " + color + ", " + age + "}";
+    }
+}
